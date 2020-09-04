7e41005d35e70804f74014de099e6c8f62327f97
Bukach Andrew <bukach.andrew93@gmail.com>
2020-09-04 16:47:14 +0600

    gits

--- resources/views/orda/layouts/footer.blade.php
+++ resources/views/orda/layouts/footer.blade.php
@@ -18,7 +18,7 @@
         </div>
     </footer>
 
-
+<!-- -->
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="/js/swiper-bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
