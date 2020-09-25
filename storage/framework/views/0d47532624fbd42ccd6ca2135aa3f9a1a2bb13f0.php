618209ff8d95db2149104186c6a01f478ace3c10
Kanat2 <2tanak@mail.ru>
2020-09-25 22:09:03 +0600

    '25-09-20-22:08'

--- storage/framework/views/0d47532624fbd42ccd6ca2135aa3f9a1a2bb13f0.php
+++ storage/framework/views/0d47532624fbd42ccd6ca2135aa3f9a1a2bb13f0.php
@@ -22,6 +22,44 @@
     <?php $__env->startSection('css_block'); ?>
     <?php echo $__env->yieldSection(); ?>
     
+	<script type="text/javascript" src="/admin-asset/assets/js/plugins/loaders/pace.min.js"></script>
+	<script type="text/javascript" src="/admin-asset/assets/js/core/libraries/jquery.min.js"></script>
+	<script type="text/javascript" src="/admin-asset/assets/js/core/libraries/bootstrap.min.js"></script>
+	<script type="text/javascript" src="/admin-asset/assets/js/plugins/loaders/blockui.min.js"></script>
+	<script type="text/javascript" src="/admin-asset/assets/js/plugins/forms/selects/select2.min.js"></script>
+	
+
+	<script type="text/javascript" src="/admin-asset/assets/js/plugins/editors/wysihtml5/wysihtml5.min.js"></script>
+	<script type="text/javascript" src="/admin-asset/assets/js/plugins/editors/wysihtml5/toolbar.js"></script>
+	<script type="text/javascript" src="/admin-asset/assets/js/plugins/editors/wysihtml5/parsers.js"></script>
+	<script type="text/javascript" src="/admin-asset/assets/js/plugins/editors/wysihtml5/locales/bootstrap-wysihtml5.ua-UA.js"></script>
+	<script type="text/javascript" src="/admin-asset/assets/js/pages/editor_wysihtml5.js"></script>
+	
+	<script type="text/javascript" src="/admin-asset/assets/js/plugins/forms/styling/uniform.min.js"></script>
+	
+	<script type="text/javascript" src="/admin-asset/assets/js/plugins/uploaders/fileinput/plugins/purify.min.js"></script>
+	<script type="text/javascript" src="/admin-asset/assets/js/plugins/uploaders/fileinput/plugins/sortable.min.js"></script>
+	<script type="text/javascript" src="/admin-asset/assets/js/plugins/uploaders/fileinput/fileinput.min.js"></script>
+	<script type="text/javascript" src="/admin-asset/assets/js/plugins/notifications/pnotify.min.js"></script>
+    
+	<script type="text/javascript" src="/admin-asset/assets/js/core/app.js"></script>
+	<script type="text/javascript" src="/admin-asset/assets/js/plugins/ui/ripple.min.js"></script>
+	<script type="text/javascript" src="/vendor/sweetalert.min.js"></script>
+	<script type="text/javascript" src="/vendor/footable.min.js"></script>
+	
+	<!------
+	<script type="text/javascript" src="/admin-asset/custom/js/main.js"></script>
+	------->
+
+<script src="/ckeditor/ckeditor.js" 
+type="text/javascript" charset="utf-8" >
+</script>
+
+    <?php $__env->startSection('js_block'); ?>
+    <?php echo $__env->yieldSection(); ?>
+		<script type="text/javascript" src="/admin-asset/drobsone/js/dropzone.js"></script>
+		
+		
 </head>
 <body>
 
