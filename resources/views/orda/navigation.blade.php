71d1018a2605415cf93e91510984e7ed6073c810
Bukach Andrew <bukach.andrew93@gmail.com>
2020-10-13 14:26:07 +0600

    header

--- resources/views/orda/navigation.blade.php
+++ resources/views/orda/navigation.blade.php
@@ -14,8 +14,8 @@
                             <input type="text" value="" placeholder="Поиск" name="s" id="s">
                             <button type="submit">
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke="#006AAB"></path>
-                                    <path d="M21 21L16.65 16.65" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke="#006AAB"></path>
+                                    <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke="#0e4f32"></path>
+                                    <path d="M21 21L16.65 16.65" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke="#0e4f32"></path>
                                 </svg>
                             </button>
                         </form>
