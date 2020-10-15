aa340df89e51971c823cb424e6b174bec25a23ef
Bukach Andrew <bukach.andrew93@gmail.com>
2020-10-15 12:48:41 +0600

    login

--- resources/views/orda/user/layouts/app.blade.php
+++ resources/views/orda/user/layouts/app.blade.php
@@ -11,41 +11,52 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-	    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="/css/style.css?v=3.5.3">
+    <link rel="stylesheet" href="/css/media.css?v=3.5.3">
+    
+    <link rel="stylesheet" href="/css/login-style.css?v=3.5.3">
+    <link rel="stylesheet" href="/css/login-media.css?v=3.5.3">
 
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
+    <div class="register__page">
+
+        <div class="register__page--header">
             <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-						
-						
-                    </a>
+                <div class="register__header--row">
+                    <div class="register__header--logo">
+                        <a href="/">
+                            <img src="/img/logo-footer.svg" alt="">
+                        </a>
+                    </div>
+    
+                    <div class="header__lang register__header--lang">
+                        <ul class="lang__menu">
+                          <li><a class='current' href="#">
+                          {{ $q_lang->get() ? $q_lang->get() : 'ru'}}
+                          </a>
+                              <ul class="lang__menu--children">
+                              @foreach ($q_lang->getAr() as $k => $v)
+                                <li><a id="{{$k}}" href="/{{$k}}/change_lang">{{$v}}</a></li>
+                              @endforeach
+                              </ul>
+                          </li>
+                      </ul>
+                    </div>
+    
                 </div>
-
-                
             </div>
-        </nav>
+        </div>
 
         @yield('content')
+
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/bootstrap.js') }}"></script>
-	    <script src="{{ asset('js/app.js') }}"></script>
+    <!--<script src="{{ asset('js/bootstrap.js') }}"></script>-->
+    <script src="{{ asset('js/app.js') }}"></script>
 
 </body>
 </html>
--- resources/views/orda/user/login.blade.php
+++ resources/views/orda/user/login.blade.php
@@ -1,33 +1,5 @@
 
-
-<div class="register__page">
-
-    <div class="register__page--header">
-        <div class="container">
-            <div class="register__header--row">
-                <div class="register__header--logo">
-                    <a href="/">
-                        <img src="/img/logo-footer.svg" alt="">
-                    </a>
-                </div>
-
-                <div class="header__lang register__header--lang">
-                    <ul class="lang__menu">
-                      <li><a class='current' href="#">
-                      {{ $q_lang->get() ? $q_lang->get() : 'ru'}}
-                      </a>
-                          <ul class="lang__menu--children">
-                          @foreach ($q_lang->getAr() as $k => $v)
-                            <li><a id="{{$k}}" href="/{{$k}}/change_lang">{{$v}}</a></li>
-                          @endforeach
-                          </ul>
-                      </li>
-                  </ul>
-                </div>
-
-            </div>
-        </div>
-    </div>
+    
 
     <div class="register__page--body">
         <div class="container">
@@ -85,67 +57,4 @@
 
         </div>
     </div>
-
-</div>
-
-
-
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Авторизация</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('check_login') }}">
-                        {{ csrf_field() }}
-                        
-                        
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-								
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Пароль</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-
-                      <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    авторизация
-                                </button>
-								
-						<p><a href ="{{route('registration')}}">регистрация</a></p>
-							
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
 
