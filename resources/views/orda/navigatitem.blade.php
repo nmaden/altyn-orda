1f2bce5effff0a1b91388b7841d2380962cb3552
Bukach Andrew <bukach.andrew93@gmail.com>
2020-10-15 12:02:44 +0600

    header

--- resources/views/orda/navigatitem.blade.php
+++ resources/views/orda/navigatitem.blade.php
@@ -2,9 +2,7 @@
   @foreach($items as $item)
 
     <li>
-        <a class="{{$item->hasChildren() ? 'menu__click':''}}"
-          data-child="{{$item->hasChildren() ? '1':''}}"
-          style="color:{{
+        <a style="color:{{
             URL::current() == UrlReplace::get($item->url()) ||
                 URL::current()  == strpos(URL::current(), UrlReplace::get($item->url()).'-item')
             ? '#B77F04' : ''}}"
