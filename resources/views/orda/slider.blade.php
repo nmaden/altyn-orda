a99ed181fa40c96e122c602c1764fd088dc57c71
Bukach Andrew <bukach.andrew93@gmail.com>
2020-10-18 12:26:47 +0600

    video

--- resources/views/orda/slider.blade.php
+++ resources/views/orda/slider.blade.php
@@ -1,36 +1,35 @@
     <div class="section__banner">
-        <div class="banner__slider">
+        <!--<div class="banner__slider">
             <div class="swiper-wrapper">
-		@if(isset($sliders))
-         @foreach($sliders->sliders as $slider)
+                @if(isset($sliders))
+                    @foreach($sliders->sliders as $slider)
 
-                <div class="swiper-slide">
-                    <div class="banner__item">
+                        <div class="swiper-slide">
+                            <div class="banner__item">
 
-                        <div class="banner__item--img">
-                 <img src="{{URL::asset($slider->photo)}}" alt="">
-                        </div>
-                        <div class="banner__item--container">
-                            <div class="container">
-                                <div class="banner__item--info">
-                                    <div class="banner__item--title">
-									{{$slider->name}}
-									
-                                    </div>
-                                    <div class="banner__item--desc">
-                                       {{$slider->description}}
+                                <div class="banner__item--img">
+                        <img src="{{URL::asset($slider->photo)}}" alt="">
+                                </div>
+                                <div class="banner__item--container">
+                                    <div class="container">
+                                        <div class="banner__item--info">
+                                            <div class="banner__item--title">
+                                            {{$slider->name}}
+                                            
+                                            </div>
+                                            <div class="banner__item--desc">
+                                            {{$slider->description}}
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
+
                             </div>
                         </div>
 
-                    </div>
-                </div>
-
-@endforeach
-@endif
+                    @endforeach
+                @endif
             </div>
-            <!-- Add Pagination -->
 			@if(count($sliders->sliders) > 1 )
             <div class="banner__slider--pag">
                 <div class="container">
@@ -51,5 +50,13 @@
                 </div>
             </div>
             
+        </div>-->
+
+        <div class="bg__video__intro">
+            <video width="100%" height="100%" loop="" muted="" id="jsvideo" autoplay="">
+                <source src="/img/video/home_banner.mp4" type="video/mp4">
+            </video>
         </div>
+
+        
     </div>
