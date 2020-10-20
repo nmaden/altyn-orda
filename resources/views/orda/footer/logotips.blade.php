e0d71ad26ad636955ab36c9b4c6bbf5a3487ff2e
Bukach Andrew <bukach.andrew93@gmail.com>
2020-10-20 11:33:22 +0600

    test

--- resources/views/orda/footer/logotips.blade.php
+++ resources/views/orda/footer/logotips.blade.php
@@ -1,11 +1,10 @@
 @if(is_array($social))
 	
   @foreach($social as $v)
-    <div class="footer__social--item tooltip__item title="текст"">
+    <div class="footer__social--item tooltip__item" title="текст">
       <a href="{{$v['name']}}">
         <img src="{{URL::asset($v['photo'])}}"  alt="">
       </a>
-      <!-- <span class="tooltip__content">текст</span>--> 
     </div>
   @endforeach
 
--- resources/views/orda/navigation.blade.php
+++ resources/views/orda/navigation.blade.php
@@ -50,20 +50,18 @@
 					
 					@if(is_array($social))
                       @foreach($social as $v)
-                        <div class="header__social--item tooltip__item">
+                        <div class="header__social--item tooltip__item" title="текст">
                             <a href="{{$v['name']}}">
                                 <img src="{{URL::asset($v['photo'])}}" alt="">
                             </a>
-                            <span class="tooltip__content">текст</span>
                         </div>
                       @endforeach
                      @endif
                     </div>
-                    <div class="header__user tooltip__item">
+                    <div class="header__user tooltip__item" title="текст">
                         <a href="{{route('login')}}">
                             <img src="/img/icon-user.svg" alt="">
                         </a>
-                        <span class="tooltip__content">текст</span>
                     </div>
                     <div class="header__menu">
                         <div class="header__menu--burger header__menu--click">
