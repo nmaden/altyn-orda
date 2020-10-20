9b40015ac00cca4773ddb351b528b5144d7faabc
Bukach Andrew <bukach.andrew93@gmail.com>
2020-10-20 11:44:02 +0600

    tooltip

--- resources/views/orda/footer/logotips.blade.php
+++ resources/views/orda/footer/logotips.blade.php
@@ -1,8 +1,8 @@
 @if(is_array($social))
 	
   @foreach($social as $v)
-    <div class="footer__social--item">
-      <a href="{{$v['name']}}" class="tooltip__item" title="текст">>
+    <div class="footer__social--item tooltip__item" title="текст">
+      <a href="{{$v['name']}}" >
         <img src="{{URL::asset($v['photo'])}}"  alt="">
       </a>
     </div>
