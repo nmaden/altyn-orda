158b635de07b30a2c5ff42902784f3646e637c7c
Bukach Andrew <bukach.andrew93@gmail.com>
2020-10-20 11:25:55 +0600

    test

--- resources/views/orda/footer/footer.blade.php
+++ resources/views/orda/footer/footer.blade.php
@@ -75,6 +75,7 @@
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <script>
     $( function() {
         $( '.tooltip__item' ).tooltip({
