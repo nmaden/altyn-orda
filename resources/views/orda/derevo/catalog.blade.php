0b43ba25865b6a7cfd61586579160f5cf79b9365
Bukach Andrew <bukach.andrew93@gmail.com>
2020-11-12 14:26:17 +0600

    derevo__block

---
+++ resources/views/orda/derevo/catalog.blade.php
@@ -0,0 +1,32 @@
+
+
+
+    <div class="about__desc page__description">
+        <div class="container">
+
+            <div class="section__title--block">
+                <h1 class="section__title derevo__title">
+                    Генеалогические дерево
+                </h1>
+            </div>
+
+
+            <div class="derevo__block">
+
+                <div class="derevo__block--img">
+                    <img src="/img/derevo-img.png" alt="">
+                </div>
+
+                <div class="derevo__block--tree">
+
+                    12
+
+                </div>
+
+
+
+            </div>
+
+        </div>
+    </div>
+
